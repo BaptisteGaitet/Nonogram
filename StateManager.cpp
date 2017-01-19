03dfba626b3a81f6049d2ca745fe71c1ece7c9c4
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-19 14:22:38 +0100

    Adds StateManager class

---
+++ StateManager.cpp
@@ -0,0 +1,58 @@
+#include "StateManager.h"
+
+StateManager::StateManager()
+{
+	// Initialize current state to an empty string
+	currentState = "";
+}
+
+void StateManager::update()
+{
+	// If the current state is valid
+	if (hasState(currentState))
+	{
+		// Update it
+		states.at(currentState)->update();
+	}
+}
+
+void StateManager::draw(sf::RenderWindow* window)
+{
+	// If the current state is valid
+	if (hasState(currentState))
+	{
+		// Draw it
+		states.at(currentState)->draw(window);
+	}
+}
+
+void StateManager::addState(State* _state)
+{
+	// Insert a new state to the map, whith its id as string key
+	states.insert(std::pair<std::string, State*>(_state->getId(), _state));
+}
+
+bool StateManager::hasState(std::string _id)
+{
+	// Declare return value as strue;
+	bool valid = true;
+
+	// Try to access the corresponding state
+	try
+	{
+		states.at(_id);
+	}
+	catch (const std::out_of_range& oor)
+	{
+		// Set return value to false
+		valid = false;
+		// Log the error
+		std::cerr << "Out of Range error:" << oor.what() << "\n";
+	}
+
+	return valid;
+}
+
+StateManager::~StateManager()
+{
+}
---
+++ StateManager.h
@@ -0,0 +1,30 @@
+#ifndef STATE_MANAGER_H
+#define STATE_MANAGER_H
+
+#include <string>
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include "State.h"
+
+/*
+This class holds several states, updating and drawing the current one and handling the switch to another state.
+*/
+
+class StateManager
+{
+private:
+	std::map<std::string, State*> states;
+	std::string currentState;
+
+	bool hasState(std::string _id);
+public:
+	StateManager();
+
+	void update();
+	void draw(sf::RenderWindow* window);
+	void addState(State* _state);
+
+	~StateManager();
+};
+
+#endif
