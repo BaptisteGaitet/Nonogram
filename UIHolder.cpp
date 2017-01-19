05b3444c0799c803e1d152342367528c98b21b5c
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-19 17:08:09 +0100

    Creates UIHolder class

---
+++ UIHolder.cpp
@@ -0,0 +1,84 @@
+#include "UIHolder.h"
+
+UIHolder::UIHolder()
+{
+}
+
+void UIHolder::update()
+{
+	for (std::map<std::string, UIElement*>::iterator it = elements.begin(); it != elements.end(); ++it)
+	{
+		it->second->update();
+	}
+}
+
+void UIHolder::draw(sf::RenderWindow* window)
+{
+	for (int i = 0; i < drawingQueue.size(); i++)
+	{
+		drawingQueue.at(i)->draw(window);
+	}
+}
+
+void UIHolder::addUIElement(UIElement* _uielement, int _drawOrder)
+{
+	elements.insert(std::pair<std::string, UIElement*>(_uielement->getId(), _uielement));
+	drawingQueue.push_back(_uielement);
+	sortDrawOrderVector();
+}
+
+UIElement* UIHolder::getUIElement(std::string _id)
+{
+	UIElement* res = nullptr;
+
+	if (hasUIElement(_id))
+	{
+		res = elements.at(_id);
+	}
+
+	return res;
+}
+
+bool UIHolder::hasUIElement(std::string _id)
+{
+	// Declare return value as strue;
+	bool valid = true;
+
+	// Try to access the corresponding UIElement
+	try
+	{
+		elements.at(_id);
+	}
+	catch (const std::out_of_range& oor)
+	{
+		// Set return value to false
+		valid = false;
+		// Log the error
+		std::cerr << "UIHolder Out of Range error: " << oor.what() << "\n";
+	}
+
+	return valid;
+}
+
+void UIHolder::sortDrawOrderVector()
+{
+	bool sorted = false;
+	while (!sorted)
+	{
+		sorted = true;
+		for (int i = 0; i < drawingQueue.size() - 1; i++)
+		{
+			if (drawingQueue.at(i)->getDrawOrder() < drawingQueue.at(i + 1)->getDrawOrder())
+			{ 
+				UIElement* tmp = drawingQueue.at(i);
+				drawingQueue.at(i) = drawingQueue.at(i + 1);
+				drawingQueue.at(i + 1) = tmp;
+				sorted = false;
+			}
+		}
+	}
+}
+
+UIHolder::~UIHolder()
+{
+}
---
+++ UIHolder.h
@@ -0,0 +1,28 @@
+#ifndef UI_HOLDER
+#define UI_HOLDER
+
+#include <SFML\Graphics.hpp>
+#include "UIElement.h"
+#include <map>
+#include <string>
+#include <iostream>
+
+class UIHolder
+{
+private:
+	std::vector<UIElement*> drawingQueue;
+	std::map<std::string, UIElement*> elements;
+public:
+	UIHolder();
+
+	void draw(sf::RenderWindow* window);
+	void update();
+	void addUIElement(UIElement* _uielement, int _drawOrder);
+	UIElement* getUIElement(std::string _id);
+	bool hasUIElement(std::string _id);
+	void sortDrawOrderVector();
+
+	~UIHolder();
+};
+
+#endif
