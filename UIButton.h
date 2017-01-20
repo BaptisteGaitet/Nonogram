45ab846c57959a090512bc33880865f56be040c4
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-20 15:52:58 +0100

    Also adds button logic

--- UIButton.h
+++ UIButton.h
@@ -3,10 +3,16 @@
 
 #include "UIElement.h"
 #include <SFML/Graphics.hpp>
+#include "MouseManager.h"
+#include "TextureManager.h"
+
+enum ButtonState {idle, hovered, pressed};
 
 class UIButton :
 	public UIElement
 {
+private:
+	ButtonState state;
 public:
 	UIButton(std::string _id, sf::IntRect _bounds, int _drawOrder, std::string _path);
 
