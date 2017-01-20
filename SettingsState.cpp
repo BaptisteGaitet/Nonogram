fa0314c46f9bd9eaf299322fb2215ab24ae715c1
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-20 17:08:31 +0100

    Adds SettingState class

---
+++ SettingsState.cpp
@@ -0,0 +1,39 @@
+#include "SettingsState.h"
+
+
+SettingsState::SettingsState() : State("settings")
+{
+	// TEST //
+
+	UIBackground* bg = new UIBackground("background", 0, "bg1.png");
+	uiholder.addUIElement(bg);
+	UIImage* img = new UIImage("image", sf::IntRect(0, 0, 800, 600), 1, "book.png");
+	uiholder.addUIElement(img);
+	UIButton* btnBack = new UIButton("btn_back", sf::IntRect(100, 500, 64, 64), 2, "btn4.png");
+	uiholder.addUIElement(btnBack);
+
+	//////////
+}
+
+// Updates the state
+void SettingsState::update()
+{
+	uiholder.update();
+
+	UIButton* btnBack = (UIButton*)uiholder.getUIElement("btn_back");
+	if (btnBack->getClic())
+	{
+		setNextStateId("title");
+	}
+}
+
+// Draws the state
+void SettingsState::draw(sf::RenderWindow* window)
+{
+	uiholder.draw(window);
+}
+
+
+SettingsState::~SettingsState()
+{
+}
---
+++ SettingsState.h
@@ -0,0 +1,20 @@
+#ifndef SETTINGS_STATE_H
+#define SETTINGS_STATE_H
+
+#include "State.h"
+#include "UIBackground.h"
+#include "UIImage.h"
+#include "UIButton.h"
+
+class SettingsState : public State
+{
+public:
+	SettingsState();
+
+	void update();
+	void draw(sf::RenderWindow* window);
+
+	~SettingsState();
+};
+
+#endif
