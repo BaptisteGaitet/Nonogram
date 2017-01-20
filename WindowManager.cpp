6bbd16548a29caf15ee3d63e65ff822339301787
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-20 15:52:26 +0100

    Creates WindowManager class

---
+++ WindowManager.cpp
@@ -0,0 +1,55 @@
+#include "WindowManager.h"
+
+
+WindowManager::WindowManager()
+{
+}
+
+WindowManager& WindowManager::getInstance()
+{
+	static WindowManager instance;
+
+	return instance;
+}
+
+void WindowManager::updateDisplayArea(sf::Vector2u screensize)
+{
+	screenSize = screensize;
+	if (screensize.x > screensize.y)
+	{
+		stretchRatio = (float)((float)screensize.y / (float)BASE_SCREEN_SIZE.y);
+		displayArea.width = BASE_SCREEN_SIZE.x * stretchRatio;
+		displayArea.height = BASE_SCREEN_SIZE.y * stretchRatio;
+		displayArea.left = (float)(screensize.x - displayArea.width) / 2;
+		displayArea.top = 0;
+	}
+	else
+	{
+		stretchRatio = (float)screensize.x / (float)BASE_SCREEN_SIZE.x;
+
+		displayArea.width = BASE_SCREEN_SIZE.x * stretchRatio;
+		displayArea.height = BASE_SCREEN_SIZE.y * stretchRatio;
+		displayArea.left = 0;
+		displayArea.top = (float)(screensize.y - displayArea.height) / 2;
+	}
+	
+}
+
+float WindowManager::getStretchRatio()
+{
+	return stretchRatio;
+}
+
+sf::IntRect WindowManager::getDisplayArea()
+{
+	return displayArea;
+}
+
+sf::Vector2u WindowManager::getScreenSize()
+{
+	return screenSize;
+}
+
+WindowManager::~WindowManager()
+{
+}
---
+++ WindowManager.h
@@ -0,0 +1,30 @@
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
+
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
+const sf::Vector2i BASE_SCREEN_SIZE = sf::Vector2i(800,600);
+
+class WindowManager
+{
+private:
+	WindowManager();
+	WindowManager(WindowManager const&);
+	void operator=(WindowManager const&);
+
+	sf::IntRect displayArea;
+	float stretchRatio;
+	sf::Vector2u screenSize;
+public:
+	static WindowManager& getInstance();
+
+	void updateDisplayArea(sf::Vector2u screenSize);
+	sf::Vector2u getScreenSize();
+	float getStretchRatio();
+	sf::IntRect getDisplayArea();
+
+	~WindowManager();
+};
+
+#endif
