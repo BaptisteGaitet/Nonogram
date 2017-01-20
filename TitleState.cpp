2fd2674ed7cc7309143826de91517f7bb23a067e
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-20 15:54:25 +0100

    Adds test code for titlescreen

--- TitleState.cpp
+++ TitleState.cpp
@@ -6,8 +6,14 @@ TitleState::TitleState() : State("title")
 	// TEST //
 	UIBackground* bg = new UIBackground("background", 0, "bg2.png");
 	uiholder.addUIElement(bg);
-	UIImage* img = new UIImage("image", sf::IntRect(50, 5, 45, 90), 1, "cover.png");
+	UIImage* img = new UIImage("image", sf::IntRect(400, 0, 400, 600), 1, "cover.png");
 	uiholder.addUIElement(img);
+	UIButton* btnStart = new UIButton("btn_start", sf::IntRect(550, 300, 128, 64), 2, "btn1.png");
+	uiholder.addUIElement(btnStart);
+	UIButton* btnSettings = new UIButton("btn_settings", sf::IntRect(550, 380, 128, 64), 2, "btn2.png");
+	uiholder.addUIElement(btnSettings);
+	UIButton* btnExit = new UIButton("btn_exit", sf::IntRect(550, 460, 128, 64), 2, "btn3.png");
+	uiholder.addUIElement(btnExit);
 	//////////
 }
 
--- TitleState.h
+++ TitleState.h
@@ -5,6 +5,7 @@
 #include "State.h"
 #include "UIBackground.h"
 #include "UIImage.h"
+#include "UIButton.h"
 
 class TitleState : public State
 {
