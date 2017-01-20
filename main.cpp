9e5f2da6ffdfd11aab5c3de995fc53de7ab8cca2
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-20 15:54:38 +0100

    Adds mouse & window manager

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include "StateManager.h"
 #include "TitleState.h"
+#include "MouseManager.h"
+#include "WindowManager.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Picr-Os");
 	window.setFramerateLimit(60);
 
+	MouseManager::getInstance().setWindow(&window);
+	WindowManager::getInstance().updateDisplayArea(window.getSize());
 	StateManager stateManager;
 	TitleState* titleState = new TitleState();
 	stateManager.addState(titleState);
@@ -18,9 +22,18 @@ int main()
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+			}
+			if (event.type == sf::Event::Resized)
+			{
+				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+				WindowManager::getInstance().updateDisplayArea(window.getSize());
+			}
 		}
 
+		MouseManager::getInstance().update();
 		stateManager.update();
 
 		window.clear();
