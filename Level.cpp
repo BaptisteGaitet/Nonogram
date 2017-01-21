6890711f43d754df72ae0780ead55e075eb919be
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-21 19:09:57 +0100

    Creates Level class

---
+++ Level.cpp
@@ -0,0 +1,53 @@
+#include "Level.h"
+
+Level::Level(std::string _imagePath, std::string _description, Nonogram _nonogram, bool _beaten)
+{
+	imagePath = _imagePath;
+	description = _description;
+	nonogram = _nonogram;
+	beaten = _beaten;
+}
+
+std::string Level::getImagePath()
+{
+	return imagePath;
+}
+
+void Level::setImagePath(std::string _imagePath)
+{
+	imagePath = _imagePath;
+}
+
+std::string Level::getDescription()
+{
+	return description;
+}
+
+void Level::setDescription(std::string _description)
+{
+	description = _description;
+}
+
+Nonogram Level::getNonogram()
+{
+	return nonogram;
+}
+
+void Level::setNonogram(Nonogram _nonogram)
+{
+	nonogram = _nonogram;
+}
+
+bool Level::getBeaten()
+{
+	return beaten;
+}
+
+void Level::setBeaten(bool _beaten)
+{
+	beaten = _beaten;
+}
+
+Level::~Level()
+{
+}
---
+++ Level.h
@@ -0,0 +1,29 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include "Nonogram.h"
+#include <string>
+
+class Level
+{
+private:
+	std::string imagePath;
+	std::string description;
+	Nonogram nonogram;
+	bool beaten;
+public:
+	Level(std::string _imagePath, std::string _description, Nonogram _nonogram, bool _beaten);
+
+	std::string getImagePath();
+	void setImagePath(std::string _imagePath);
+	std::string getDescription();
+	void setDescription(std::string _description);
+	Nonogram getNonogram();
+	void setNonogram(Nonogram _nonogram);
+	bool getBeaten();
+	void setBeaten(bool _beaten);
+
+	~Level();
+};
+
+#endif
