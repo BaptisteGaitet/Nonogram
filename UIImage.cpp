f7a71081cbe1a9ff891ea667a4828addb247f204
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-21 16:00:12 +0100

    Adds enable/disable option

--- UIImage.cpp
+++ UIImage.cpp
@@ -3,6 +3,7 @@
 
 UIImage::UIImage(std::string _id, sf::IntRect _bounds, int _drawOrder, std::string _path) : UIElement(_id, _bounds, _drawOrder)
 {
+	enabled = true;
 	TextureManager::getInstance().addTexture(_path);
 	shape.setTexture(TextureManager::getInstance().getTexture(_path));
 	shape.setTextureRect(sf::IntRect(0, 0, TextureManager::getInstance().getTexture(_path)->getSize().x, TextureManager::getInstance().getTexture(_path)->getSize().y));
@@ -16,9 +17,22 @@ void UIImage::update()
 
 void UIImage::draw(sf::RenderWindow* window)
 {
-	snapToDisplayArea();
+	if (enabled)
+	{
+		snapToDisplayArea();
 
-	window->draw(shape);
+		window->draw(shape);
+	}
+}
+
+bool UIImage::getEnabled()
+{
+	return enabled;
+}
+
+void UIImage::setEnabled(bool _val)
+{
+	enabled = _val;
 }
 
 UIImage::~UIImage()
--- UIImage.h
+++ UIImage.h
@@ -9,11 +9,15 @@
 class UIImage :
 	public UIElement
 {
+private:
+	bool enabled;
 public:
 	UIImage(std::string _id, sf::IntRect _bounds, int _drawOrder, std::string _path);
 
 	void update();
 	void draw(sf::RenderWindow* window);
+	bool getEnabled();
+	void setEnabled(bool _val);
 
 	~UIImage();
 };
