d6206dece498438675952b705711e2eb3c882722
BaptisteGaitet <baptiste.gaitet@gmail.com>
2017-01-22 17:04:23 +0100

    Adds UI logic for lvlButtons, level description and level image

--- LevelSelectState.cpp
+++ LevelSelectState.cpp
@@ -4,6 +4,7 @@
 LevelSelectState::LevelSelectState() : State("levelselect")
 {
 	selectedLevel = 0;
+	currentPage = 0;
 
 	// TEST //
 	sf::Vector2i buttonMargin = sf::Vector2i(120, 120);
@@ -15,6 +16,10 @@ LevelSelectState::LevelSelectState() : State("levelselect")
 			std::string name = "lvlButton" + std::to_string(j * 3 + i);
 			UILevelButton* lvlButton = new UILevelButton(name, sf::IntRect((i * (64 + buttonOffset.x)) + buttonMargin.x, (j * (64 + buttonOffset.y)) + buttonMargin.y, 64, 64), 3);
 			lvlButton->setLevelNumber(j * 3 + i);
+			if (j * 3 + i == 0)
+			{
+				lvlButton->setSelected(true);
+			}
 			uiholder.addUIElement(lvlButton);
 		}
 	}
@@ -30,6 +35,8 @@ LevelSelectState::LevelSelectState() : State("levelselect")
 	UIButton* btnRight = new UIButton("btn_right", sf::IntRect(250, 400, 64, 64), 2, "btnArrowRight.png");
 	uiholder.addUIElement(btnRight);
 
+	UIImage* imgFrame= new UIImage("img_frame", sf::IntRect(400, 60, 300, 300), 3, "sketchFrame.png");
+	uiholder.addUIElement(imgFrame);
 	UIImage* imgSketch = new UIImage("img_sketch", sf::IntRect(400, 60, 300, 300), 2, "unknownSketch.png");
 	uiholder.addUIElement(imgSketch);
 	UILabel* lblDescription = new UILabel("lbl_description", sf::IntRect(420, 360, 100, 24), 2, "Unknown.");
@@ -37,6 +44,9 @@ LevelSelectState::LevelSelectState() : State("levelselect")
 	UIButton* btnSolve = new UIButton("btn_solve", sf::IntRect(550, 500, 128, 64), 2, "btnSolve.png");
 	uiholder.addUIElement(btnSolve);
 	//////////
+
+	updateLevelInfo();
+	updateLevelButtons();
 }
 
 void LevelSelectState::update()
@@ -57,12 +67,27 @@ void LevelSelectState::update()
 		{
 			selectedLevel = btn->getLevelNumber();
 			btn->setSelected(true);
-			std::cout << std::to_string(selectedLevel) << "\n";
+			updateLevelInfo();
 		}
 		else if (btn->getLevelNumber() != selectedLevel)
 		{
 			btn->setSelected(false);
 		}
+	}
+
+	UIButton* btnLeft = (UIButton*)uiholder.getUIElement("btn_left");
+	UIButton* btnRight = (UIButton*)uiholder.getUIElement("btn_right");
+	if (btnLeft->getClic())
+	{
+		currentPage--;
+		updateLevelButtons();
+		std::cout << currentPage << "\n";
+	}
+	if (btnRight->getClic())
+	{
+		currentPage++;
+		updateLevelButtons();
+		std::cout << currentPage << "\n";
 	}
 }
 
@@ -71,9 +96,53 @@ void LevelSelectState::draw(sf::RenderWindow* window)
 	uiholder.draw(window);
 }
 
+void LevelSelectState::updateLevelInfo()
+{
+	if (LevelManager::getInstance().getLevel(selectedLevel)->getBeaten())
+	{
+		UIImage* img = (UIImage*)uiholder.getUIElement("img_sketch");
+		img->changeImage(LevelManager::getInstance().getLevel(selectedLevel)->getImagePath());
+		UILabel* lbl = (UILabel*)uiholder.getUIElement("lbl_description");
+		lbl->setString(LevelManager::getInstance().getLevel(selectedLevel)->getDescription());
+	}
+	else
+	{
+		UIImage* img = (UIImage*)uiholder.getUIElement("img_sketch");
+		img->changeImage("unknownSketch.png");
+		UILabel* lbl = (UILabel*)uiholder.getUIElement("lbl_description");
+		lbl->setString("Unknown.");
+	}
+}
+
 void LevelSelectState::updateLevelButtons()
 {
+	UIButton* btnLeft = (UIButton*)uiholder.getUIElement("btn_left");
+	UIButton* btnRight = (UIButton*)uiholder.getUIElement("btn_right");
+	btnLeft->setEnabled(true);
+	btnRight->setEnabled(true);
+	if (currentPage == 0)
+	{
+		btnLeft->setEnabled(false);
+	}
+	if (!LevelManager::getInstance().hasLevel((currentPage + 1) * LEVELS_PER_PAGE))
+	{
+		btnRight->setEnabled(false);
+	}
 
+	for (int i = 0; i < 9; i++)
+	{
+		std::string btnId = "lvlButton" + std::to_string(i);
+		UILevelButton* btn = (UILevelButton*)uiholder.getUIElement(btnId);
+		btn->setLevelNumber(currentPage * LEVELS_PER_PAGE + i);
+		if (!LevelManager::getInstance().hasLevel(btn->getLevelNumber()))
+		{
+			btn->setEnabled(false);
+		}
+		else
+		{
+			btn->setEnabled(true);
+		}
+	}
 }
 
 LevelSelectState::~LevelSelectState()
--- LevelSelectState.h
+++ LevelSelectState.h
@@ -7,12 +7,17 @@
 #include "UIButton.h"
 #include "UILabel.h"
 #include "UILevelButton.h"
+#include "LevelManager.h"
 
 class LevelSelectState : public State
 {
 private:
+	const int LEVELS_PER_PAGE = 9;
+	
 	int selectedLevel;
+	int currentPage;
 
+	void updateLevelInfo();
 	void updateLevelButtons();
 public:
 	LevelSelectState();
